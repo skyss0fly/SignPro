d3ad94746f623e90c8ba0dc717baf0ede7565c35
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-08-02 18:57:50 +1000

    Create Main.php

---
+++ skyss0fly/SignPro/Main.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace skyss0fly\SignPro;
+
+use pocketmine\plugin\PluginBase;
+
+
+ class Main extends PluginBase {
+
+
+public function onLoad(): void {
+$cfg = $this->getConfig();
+  $this->saveDefaultConfig();
+
+}
+
+public function transfercmd() {
+
+
+}
+
+
+
+
+ }
